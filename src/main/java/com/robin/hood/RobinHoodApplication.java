5d59496c6cd40e34b857e3fa07854f465b0bd690
Daniel194 <lungu.daniel94@gmail.com>
2019-04-04 22:40:18 +0300

    first commit

---
+++ src/main/java/com/robin/hood/RobinHoodApplication.java
@@ -0,0 +1,13 @@
+package com.robin.hood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinHoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinHoodApplication.class, args);
+	}
+
+}
